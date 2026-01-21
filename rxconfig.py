c715c51218dc1ba88dbbd640b8d241258dab21c9
andresok10 <davexgarcia@gmail.com>
2026-01-21 10:15:32 -0500

    guardarok

--- rxconfig.py
+++ rxconfig.py
@@ -6,6 +6,7 @@ config = rx.Config(
     app_name="p1",
     host="0.0.0.0",
     port=int(os.environ.get("PORT", 3000)),
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
 
 '''import reflex as rx
