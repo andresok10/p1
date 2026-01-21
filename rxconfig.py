97c3f061657d3cff0804710f0d512c0cda2e5c94
andresok10 <davexgarcia@gmail.com>
2026-01-21 12:40:51 -0500

    guardarok

--- rxconfig.py
+++ rxconfig.py
@@ -5,7 +5,6 @@ import os
 config = rx.Config(
     app_name="p1",
     pwa=False,
-    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
 #port=int(os.environ.get("PORT", 3000)),
 '''import reflex as rx
@@ -14,7 +13,7 @@ config = rx.Config(
 )'''
 
 #host="0.0.0.0",
-#disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
+# disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 # api_url="https://TU-BACKEND.onrender.com",
 # pip install -r requirements.txt && reflex export
 # reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT
