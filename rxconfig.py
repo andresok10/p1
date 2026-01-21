1dabd3b8f0d0ab37b21f5341099bfed508d5fd97
andresok10 <davexgarcia@gmail.com>
2026-01-21 10:08:13 -0500

    guardarok

--- rxconfig.py
+++ rxconfig.py
@@ -1,14 +1,14 @@
 # rxconfig.py
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="p1",
     host="0.0.0.0",
-    port=3000,
+    port=int(os.environ.get("PORT", 3000)),
 )
 
 '''import reflex as rx
 config = rx.Config(
     app_name="p1",
 )'''
-
